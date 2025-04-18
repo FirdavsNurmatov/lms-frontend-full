ff62f201b1bbcb83e0b47a85a952c50129902bbf
FirdavsNurmatov <nurmatovfirdavs96@gmail.com>
2025-04-18 15:48:08 +0500

    chore: login file updated and working

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 import { BrowserRouter } from "react-router";
-import QueryProviderComponent from './providers/QueryProviderComponent.tsx';
-createRoot(document.getElementById('root')!).render(
+import QueryProviderComponent from "./providers/QueryProviderComponent.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <QueryProviderComponent>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </QueryProviderComponent>
-
-)
+);
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,28 +1,39 @@
 import { useAuthStore } from "../../store/useAuthStore";
 import { instance } from "../../config/axios-instance";
 import { Button, Form, FormProps, Input } from "antd";
+import { replace, useNavigate } from "react-router-dom";
 
 const Login = () => {
   type FieldType = {
-    login?: string;
+    username?: string;
     password?: string;
   };
-
+  
   const { user, setUser, setToken, token } = useAuthStore((store) => store);
-  const loginHandler = async () => {
-    const res = await instance.post("/auth/login", {
-      username: "johndoe",
-      password: "password123",
-    });
-    console.log(res);
-
-    setUser(res.data.user);
-    setToken(res.data.data.accessToken);
-  };
-
-  const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
-    const { login, password } = values;
-    if (login && password) {
+  // const loginHandler = async () => {
+  //   const res = await instance.post("/auth/login", {
+  //     username: "johndoe",
+  //     password: "password123",
+  //   });
+  //   console.log(res);
+    
+  // };
+  
+  const navigate = useNavigate();
+  
+  const onFinish: FormProps<FieldType>["onFinish"] = async (values) => {
+    const { username, password } = values;
+    if (username && password) {
+      console.log(username, password);
+      const res = await instance.post("/auth/login", {
+        username: username,
+        password: password,
+      });
+      console.log(res.data.data.accessToken);
+      setUser(res.data.user);
+      setToken(res.data.data.accessToken);
+  
+      navigate("/dashboard", { replace: true });
     }
   };
 
@@ -33,42 +44,61 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <p>Login</p>
-      <Form
-        name="basic"
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          name={"username"}
-          rules={[{ required: true, message: "Iltimos username kiriting!" }]}
+    <div className="container">
+      <div className="login__block">
+        <p className="login__title">Tizimga kirish</p>
+        <Form
+          name="basic"
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="true"
         >
-          <Input placeholder="Login" />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: "Iltimos parolni kiriting!" }]}
-        >
-          <Input.Password
-            name="Parol"
+          <Form.Item
+            name={"username"}
+            rules={[{ required: true, message: "Iltimos username kiriting!" }]}
+          >
+            <Input
+              name="username"
+              placeholder="Username"
+              style={{
+                fontSize: "18px",
+                padding: "4px",
+              }}
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Iltimos parolni kiriting!" }]}
+          >
+            <Input.Password
+              name="password"
+              placeholder="Parol"
+              style={{
+                fontSize: "18px",
+                padding: "4px",
+              }}
+            />
+          </Form.Item>
+          <Button
+            type="primary"
             style={{
-              fontSize: "14px",
-              padding: "2px",
+              width: "100%",
+              marginTop: "20px",
+              padding: "20px",
+              fontSize: "18px",
             }}
-          />
-        </Form.Item>
-        <Button type="primary" htmlType="submit">
-          Kirish
-        </Button>
-      </Form>
-      {/* {token ? (
+            htmlType="submit"
+          >
+            Kirish
+          </Button>
+        </Form>
+        {/* {token ? (
         <p>{user.full_name}</p>
-      ) : (
-        <button onClick={loginHandler}>Login</button>
-      )}
-      <Link to="/dashboard">Dashboard</Link> */}
+        ) : (
+          <button onClick={loginHandler}>Login</button>
+          )}
+          <Link to="/dashboard">Dashboard</Link> */}
+      </div>
     </div>
   );
 };
