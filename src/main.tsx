2cd20be072df002800a0d8622704870bcb8ddde2
FirdavsNurmatov <nurmatovfirdavs96@gmail.com>
2025-05-01 15:01:48 +0500

    chore: errors fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router";
-import QueryProviderComponent from "./providers/QueryProviderComponent.tsx";
+import QueryProviderComponent from "./providers/QueryProviderComponent";
 
 createRoot(document.getElementById("root")!).render(
   <QueryProviderComponent>
